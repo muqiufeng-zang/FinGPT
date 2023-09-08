defc8a71293e054f93705354bbe2b007e7c608d9
臧朝鹏 <chaopengzang@91jkys.com>
2023-09-08 19:50:05 +0800

    优化命令

--- fingpt/FinGPT-v1/training/finetune.py
+++ fingpt/FinGPT-v1/training/finetune.py
@@ -128,7 +128,7 @@ def main():
     print("3333333")
     print(model.module)
 
-    # model = model.module.to(torch.float16)  # 将模型设置为半精度
+    model = model.module.to(torch.float16)  # 将模型设置为半精度
 
     # setup peft
     peft_config = LoraConfig(
@@ -151,7 +151,7 @@ def main():
 
     # start train
     trainer = ModifiedTrainer(
-        model=model.module,
+        model=model,
         train_dataset = train_data,
         eval_dataset = val_data,
         args=training_args,
